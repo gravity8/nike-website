155e2c6a8abe0f3d10b737d852adb31d84ec8c1d
David Adisa <adisad377@gmail.com>
2022-12-25 23:33:57 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+// change TextColors on click on color control
+
+const adaptText = document.getElementById("adapt");
+const shoeName = document.getElementById("shoeName");
+const buyNow = document.getElementById("buyNow");
+const container = document.querySelector(".active");
+const controlRight = document.getElementById("control-right");
+const controlLeft = document.getElementById("control-left");
+const containerSwitch = document.getElementById("container-Switch");
+
+
+
+
+// how to add interval to a loop in javascript?
+
+
+
+//intializing AOS LIBRARY?
+AOS.init();
+
+
+//how to loop through HTMLCOLLECTION and trigger a function?
